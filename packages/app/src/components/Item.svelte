594470baddc9dc5a3b71d243e841b3198d4dfb66
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-06-29 22:14:44 +0700

    app/feat: limit description height

--- packages/app/src/components/Item.svelte
+++ packages/app/src/components/Item.svelte
@@ -41,7 +41,7 @@ function fillHeart() {
   <div class="flex">
     <div class="flex flex-col grow pt-2 gap-2">
       <div>Title</div>
-      <div>
+      <div class="max-h-28 overflow-scroll">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </div>
     </div>
