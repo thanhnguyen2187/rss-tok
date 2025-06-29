0c8c4bb75e928e1765bb8f40bd8ff756d27d938e
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-06-29 15:55:05 +0700

    app/init: rename from `frontend`

---
+++ packages/app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+async function fetchData() {
+  const resp = await fetch("/api/data");
+  return await resp.text();
+}
+</script>
+
+<div class="prose max-w-none">
+    <p>Move to `/demo`</p>
+</div>
